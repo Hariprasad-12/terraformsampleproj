4282441537aeb938d5b15b122ce59c3a0e3d2050
Hariprasad-12 <91470332+Hariprasad-12@users.noreply.github.com>
2021-09-28 10:37:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "access_key" {
-  default = "AKIA4S3XCB3YDAMS3R7Y"
-  }
-variable "secret_key" {
-  default = "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"
-  }
+# variable "access_key" {
+#   default = "***"
+#   }
+# variable "secret_key" {
+#   default = "***"
+#   }
 variable "region" {
   default = "us-east-2"
   }
