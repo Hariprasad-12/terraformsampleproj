a6cdc80c773d7e870ca519b5a3ed2b71c8ee6f81
Hariprasad-12 <91470332+Hariprasad-12@users.noreply.github.com>
2021-09-29 12:55:20 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraformbucketsample" {
-  bucket = "my-new-terraform-bucket"
+  bucket = "my-new-terraform-bucket1"
   acl    = "private"
   tags = {
     Name        = "My bucket"
